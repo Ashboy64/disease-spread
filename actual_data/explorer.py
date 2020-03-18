dc70600277a47378415a80dc0111c2a8e2471a35
Ashish Rao <ashish.arartc@gmail.com>
2020-03-18 12:30:16 -0700

    start comparing with italy

--- actual_data/explorer.py
+++ actual_data/explorer.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def accumulate(path):
+def accumulate_actual_italy(path):
     main_df = pd.DataFrame({"Date":[], "Cases":[], "Deaths":[]})
     for fname in os.listdir(path):
 
@@ -22,7 +22,23 @@ def accumulate(path):
 
     return main_df
 
+def accumulate_simulated(path):
+    df = pd.read_csv(os.path.join(path, "log.csv"))
+    df = df.drop([col for col in list(df) if col != "dead"], axis=1)
+    df = df.loc[df["dead"] > 0].head(18)
+    df["dead"] = df["dead"].apply(lambda x: 206*x)
+    df["Deaths"] = df["dead"]
+    df = df.drop(["dead"], axis=1)
+    return df
+
 if __name__ == '__main__':
-    df = accumulate("europe")
-    df.plot(x="Date", y="Deaths")
+    df = accumulate_actual_italy("europe")
+    simulated = accumulate_simulated("../logs/log_18_03_2020_11_44_25")
+
+    df.index = simulated.index
+    df["Simulated Deaths"] = simulated["Deaths"]
+    df["Actual Deaths"] = df["Deaths"]
+    df = df.drop(["Deaths", "Cases"], axis=1)
+
+    df.plot()
     plt.show()
